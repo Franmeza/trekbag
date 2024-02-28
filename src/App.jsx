7048ac3dcf796792d9fff5810d2ded2a3bbad381
Franmeza <ing.meza.francisco@gmail.com>
2024-02-28 11:16:46 -0500

    View structure of the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import BackgroundHeading from "./components/BackgroundHeading";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import ItemList from "./components/ItemList";
+import SideBar from "./components/SideBar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BackgroundHeading />
+      <main>
+        <Header />
+        <SideBar />
+        <ItemList />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer>
+      <small>Created by Francisco Meza. </small>
+      <small>February 2024</small>
+    </footer>
+  );
+}
+
+export default Footer;
