a0e25bfae2e95d234adeaafbc1304aa3328ab50b
Franmeza <ing.meza.francisco@gmail.com>
2024-03-04 05:58:10 -0500

    date of creation changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer>
       <small>Created by Francisco Meza. </small>
-      <small>February 2024</small>
+      <small>March 2024</small>
     </footer>
   );
 }
